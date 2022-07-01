9d30d0787c1a75c402b3a55079f78ee1bc02eb83
cwseitz <cwseitz2@gmail.com>
2022-06-30 20:27:14 -0400

    update

--- mains/test.py
+++ mains/test.py
@@ -6,3 +6,10 @@ norm.test()
 
 psn = Poisson(10)
 psn.test()
+
+mu_norm = 0
+mu_psn = 50
+sigma_norm = 0.1
+psnorm = PoissonNormal(mu_norm,mu_psn,sigma_norm)
+psnorm.test()
+
--- stats/distribution.py
+++ stats/distribution.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.stats import poisson, multivariate_normal
+from scipy.stats import poisson, norm, multivariate_normal
 
 class Distribution:
     def __init__(self):
@@ -9,24 +9,25 @@ class Distribution:
 class PoissonNormal(Distribution):
     def __init__(self,mu_norm,mu_psn,sigma_norm):
         self.p = poisson(mu_psn)
-        self.g = norm(loc=mu_norm, scale=sigma_norm)
+        self.g = norm(loc=0, scale=sigma_norm)
         self.mu_norm=mu_norm
         self.mu_psn=mu_psn
         self.sigma_norm=sigma_norm
 
     def eval(self,x):
-        fp = self.p.pmf(x, self.mu_psn)
+        fp = self.p.pmf(x)
         n = x.shape[0]
         mat = np.zeros((n,n))
         for i, fp_i in enumerate(fp):
-            mat[i] = norm.pdf(x, loc=x[i], scale=sigma)*fp_i
+            mat[i] = (x[i] + self.g.pdf(x))*fp_i
         fpg = np.sum(mat,axis=0)
         return fpg
 
-    def sample(self,nsamples):
-        x = self.p.rvs(size=nsamples)
-        y = self.g.rvs(size=nsamples)
-        return np.array([x,y])
+    def test(self):
+        x = np.linspace(-100,100,1000)
+        y = self.eval(x)
+        plt.plot(x,y,color='black')
+        plt.show()
 
 class Normal(Distribution):
     def __init__(self,mu,cov):
